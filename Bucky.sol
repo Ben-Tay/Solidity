97b58686a7136a62a29f23b94e9b1185df775c8f
Benedict Tay <76094428+Ben-Tay@users.noreply.github.com>
2024-07-30 17:32:26 +0800

    Update and rename testing.sol to Bucky.sol

--- Bucky.sol
+++ Bucky.sol
@@ -2,12 +2,20 @@
 
 pragma solidity >=0.8.26;   // State version of solidity working with, must be same as compiler version used
 
-contract Ben {
+contract Bucky {
     uint age;               // uint = non-negative integer
 
+    // constructor - initialization executed as soon as contract is first created, 
+    // requires us to give an initial age (default value)
+
+    constructor (uint _age) {
+        age = _age;
+    }
+    
+
     // calling this function on the contract sends a transaction to the network
-    function setAge(uint x ) public {
-        age = x;
+    function setAge(uint _age) public {
+        age = _age;
     }
 
     function getAge() public view returns (uint) {
