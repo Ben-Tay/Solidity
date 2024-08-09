820ef092b45c7d653beb5a48c25e053b24996e59
Benedict Tay <76094428+Ben-Tay@users.noreply.github.com>
2024-08-09 18:06:27 +0800

    Update Custom.sol

--- Custom.sol
+++ Custom.sol
@@ -5,8 +5,32 @@ pragma solidity ^0.8.0
 
 contract Custom {
     address owner;
+    address public contractAddress;
+    address public payer;
+    address public origin;
     string name = " "; 
-  
+    uint public amount;
+
+    // constructor - function that is ran only once when initialized
+    constructor() {
+        contractAddress = address(this); //this is a global variable that responds to address of current smart contract
+    }
+
+    function pay() public payable {
+        payer =  msg.sender;   // msg.sender = person whos calling the function
+        origin = tx.origin;    // tx.origin = transaction origin
+        amount = msg.value;    // msg.value = amount of ether sent 
+        
+    }
+
+    function getBlockInfo() public view returns (uint, uint, uint) {
+        return (
+            block.number,         //current block on chain
+            block.timestamp,
+            block.chainid
+        );
+    }
+        
     // Solidity allows to create customer access modifiers 
     modifier onlyOwner {
         require(msg.sender == owner, 'caller must be owner');
